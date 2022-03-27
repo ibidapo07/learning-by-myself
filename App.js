2992d62dad38648df991c551fb728c5556181360
D <ibidaposmac@Fatoguns-MacBook-Pro.local>
2022-03-27 14:30:50 +0100

    testing

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Dashboard from './app/screens/Dashboard';
+
+export default function App() {
+  return (
+        <Dashboard/>
+  );
+}
+
+
